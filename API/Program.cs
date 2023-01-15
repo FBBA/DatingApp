6f287a60c55fb1047e79d7d83d69d8252c021df5
Talali <fbalashov@tistatech.com>
2023-01-15 11:30:10 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 //builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,7 +30,9 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 //app.UseAuthorization();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "http://localhost:4200"
+));
 app.MapControllers(); 
 
 app.Run();
